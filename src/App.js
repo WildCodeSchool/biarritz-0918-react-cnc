5d12fdad5f7940389fd83738c770809f5706a013
ni33wild <nicola.trentini.wild@gmail.com>
2018-11-23 18:07:07 +0100

    Added Home

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React, { Component } from 'react';
-import logo from './clic.png';
 import './App.css';
 import { Button } from 'reactstrap';
 import Navbar from './Navbar.jsx';
-import InputSearch from './components/input/Input.jsx';
-import SearchBar from './components/search/SearchBar.jsx';
-import Error from './components/Pages/Error.js';
+
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <InputSearch />
-        </header>
+        <Navbar />    
       </div>
     );
   }
