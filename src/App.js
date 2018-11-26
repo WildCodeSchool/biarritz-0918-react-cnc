c331abc1035713fc426013efdfc8796ef3bbd3cb
Karnak19 <basile64.v@gmail.com>
2018-11-26 11:28:27 +0100

    add app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Button } from 'reactstrap';
-<<<<<<< HEAD
 import Navbar from './components/navbar/Navbar.jsx';
-=======
-import Navbar from   './components/navbar/Navbar.jsx';
->>>>>>> clientprofile
 
 
 
