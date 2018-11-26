a72f1e120c92dd4c88e48da6f9dc96ce7e989621
Karnak19 <basile64.v@gmail.com>
2018-11-26 11:31:47 +0100

    fix css issue

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -15,7 +15,7 @@ import {
 
 import LoginModal from '../../LoginModal.jsx';
 import logo from '../../clic.png';
-import './Navbar.module.css';
+import styles from './Navbar.module.css';
 import Error from '../Pages/Error.js';
 import About from "../Pages/About.js"
 import Home from "../Pages/Home.js"
@@ -39,44 +39,44 @@ export default class Example extends React.Component {
     render() {
         return (
             <Router>
-            <div>
-                <Navbar color="dark" dark expand="md">
-                <img src={logo}  alt="logo" />
-                    <NavbarBrand href="/">Clic et Coupe</NavbarBrand>
-                    <NavbarToggler onClick={this.toggle} />
-                    <Collapse isOpen={this.state.isOpen} navbar>
-                        <Nav className="ml-auto" navbar>
-                            <NavItem>
-                                <NavLink tag={Link} to="/">Home</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink tag={Link} to="/about/">About</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink tag={Link} to="/cprofile/">ProfileClient</NavLink>
-                            </NavItem>
-                            <UncontrolledDropdown nav inNavbar>
-                                <DropdownToggle nav caret>
-                                    Options
+                <div>
+                    <Navbar className={styles.toto} color="dark" dark expand="md">
+                        <img src={logo} alt="logo" />
+                        <NavbarBrand href="/">Clic et Coupe</NavbarBrand>
+                        <NavbarToggler onClick={this.toggle} />
+                        <Collapse isOpen={this.state.isOpen} navbar>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <NavLink tag={Link} to="/">Home</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink tag={Link} to="/about/">About</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink tag={Link} to="/cprofile/">ProfileClient</NavLink>
+                                </NavItem>
+                                <UncontrolledDropdown nav inNavbar>
+                                    <DropdownToggle nav caret>
+                                        Options
                                 </DropdownToggle>
-                                <DropdownMenu right>
-                                    {/* Input here */}
-                                </DropdownMenu>
-                            </UncontrolledDropdown>
-                            <NavItem>
-                                <LoginModal />
-                            </NavItem>
-                        </Nav>
-                    </Collapse>
-                </Navbar>
-                {/* Routes definition of paths and related components */}
-                <Switch>
-                    <Route path="/" exact component={Home} />
-                    <Route path="/about/" component={About} />
-                    <Route path="/cprofile/" component={ProfileClient} />
-                    <Route component={Error} />
-                </Switch>
-            </div>
+                                    <DropdownMenu right>
+                                        {/* Input here */}
+                                    </DropdownMenu>
+                                </UncontrolledDropdown>
+                                <NavItem>
+                                    <LoginModal />
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </Navbar>
+                    {/* Routes definition of paths and related components */}
+                    <Switch>
+                        <Route path="/" exact component={Home} />
+                        <Route path="/about/" component={About} />
+                        <Route path="/cprofile/" component={ProfileClient} />
+                        <Route component={Error} />
+                    </Switch>
+                </div>
             </Router>
         );
     }
