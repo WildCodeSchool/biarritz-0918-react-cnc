d7ec36e1d5123f4446d1b71bf8c8a968cab9da5d
ni33wild <nicola.trentini.wild@gmail.com>
2018-11-29 23:23:20 +0100

    installed react admin for api plaform that works in local

--- src/components/Pages/AdminPanel/AdminPanel.js
+++ src/components/Pages/AdminPanel/AdminPanel.js
@@ -1,16 +1,4 @@
 import React from 'react';
-import jsonServerProvider from 'ra-data-json-server';
-import { Admin, Resource } from 'react-admin';
-import { UserList } from './Users';
+import { HydraAdmin } from '@api-platform/admin';
 
-
-
-const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
-const AdminPanel = () => (
-  <Admin dataProvider={dataProvider}>
-      {/* <Resource name="users" list={UserList} /> */}
-      <Resource name="users" list={UserList} />
-  </Admin>
-);
-
-export default AdminPanel;
+export default () => <HydraAdmin entrypoint="http://127.0.0.1:8000/api"/>;
