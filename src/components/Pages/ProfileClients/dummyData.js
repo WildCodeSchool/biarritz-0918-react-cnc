65765802bf0e430c2e5eb2742529884d50843038
ni33wild <nicola.trentini.wild@gmail.com>
2018-11-29 17:21:32 +0100

    added dummyData.js

---
+++ src/components/Pages/ProfileClients/dummyData.js
@@ -0,0 +1,16 @@
+export const userprofile = [
+    {
+      username: "user2@example.com",
+      roles: [
+        "ROLE_USER"
+      ],
+      password: "$2y$13$kSc/BLuz3PHmfHGwWog7aOP0mmBnjL7fujCZZbV.rIireKC0jpKF2",
+      salt: null,
+      id: 18,
+      name: "Jaleel",
+      surname: "user2",
+      email: "user2@example.com",
+      phone: 102261126,
+      rdvs: []
+    }
+  ];
