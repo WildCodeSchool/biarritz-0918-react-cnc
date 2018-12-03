53d212dd320458ac03b00bc957542cdc696778dc
ni33wild <nicola.trentini.wild@gmail.com>
2018-12-03 16:14:28 +0100

    merge fixed

--- src/components/Pages/ProfileClients/ProfileClient.jsx
+++ src/components/Pages/ProfileClients/ProfileClient.jsx
@@ -6,7 +6,6 @@ import styles from './ProfileClient.module.css';
 import { userprofile } from './dummyData';
 
 class ProfileClient extends Component {
-<<<<<<< HEAD
 	constructor() {
 		super();
 		this.state = {
@@ -50,55 +49,6 @@ class ProfileClient extends Component {
 			</div>
 		);
 	}
-=======
-
-  constructor() {
-    super();
-    this.state = {
-      userprofile: []
-    }
-  }
-
-  componentDidMount() {
-    this.setState({ userprofile });
-  }
-
-
-  // TO DO - MODIFY when login will be ok
-  // componentDidMount(){
-  //   /* const { id } = this.props; */
-  //   axios
-  //     //.get(`https://cnc-api.herokuapp.com/user_ids/${id}`)
-  //     .get(`https://cnc-api.herokuapp.com/user_ids/18`, { headers: { Accept: "application/json"}})
-  //     .then(res => this.setState({ userprofile: [res.data] }));
-  // }
-
-  //TO TEST AND UTILIZE WHEN API WILL BE OK
-  // handleOnSubmit(e) {
-  //   e.preventDefault();
-  //   axios
-  //     .put(`http://127.0.0.1:8001/api/directories/${id}.json`, {
-  //       name
-  //     })
-  //     .then(() => this.setState({ TO DO }));
-  // }
-
-
-  render() {
-    console.log(this.state)
-    return (
-
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-lg-8 offset-lg-2">
-            <h3>Mon compte</h3>
-            <ProfileClass profileclass={this.state.userprofile} />
-          </div>
-        </div>
-      </div>
-    );
-  }
->>>>>>> 1e82b347a0d87a41ed705196c2d25be65106bae3
 }
 
 export default ProfileClient;
