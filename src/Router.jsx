b598b3ef98066649a9a068ba636da96c64665ded
Karnak19 <basile64.v@gmail.com>
2018-12-04 11:35:12 +0100

    NICOLA renamed ProfileClient.page and added ProfileClient to Router

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 import Home from "./pages/Home.page.jsx";
 import Error from "./pages/Error.page.jsx";
+import ProfileClient from "./pages/ProfileClients/ProfileClient.page.jsx";
 
 
 export default function () {
@@ -11,8 +12,8 @@ export default function () {
 
             <Switch>
                 <Route path="/" exact component={Home} />
-                {/* <Route path="/cprofile/" component={ProfileClient} />
-                <Route
+                <Route path="/cprofile/" component={ProfileClient} />
+                {/* <Route
                     exact path="/sprofile/:id"
                     component={({ match }) => (
                         <ProfileSalon id={match.params.id} />
