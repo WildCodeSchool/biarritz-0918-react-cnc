f885932157904758f947769fc782e7a3e03b66d1
ni33wild <nicola.trentini.wild@gmail.com>
2018-12-06 12:19:05 +0100

    ProfileClient and ProfileForm updated put with id and token working

--- src/pages/ProfileClients/ProfileClient.page.jsx
+++ src/pages/ProfileClients/ProfileClient.page.jsx
@@ -4,58 +4,78 @@ import axios from 'axios';
 import ProfileForm from './ProfileForm.jsx';
 import styles from './ProfileClient.module.css';
 import ResponsiveLayout from '../../layouts/Responsive.layout.jsx';
+import * as AuthApi from '../../Auth.api.js';
+
+function getValuesFrom(...inputs) {
+  let res = {};
+  inputs.forEach((input) => input.name && (res[input.name] = input.value));
+  return res;
+}
 
 class ProfileClient extends Component {
-	constructor() {
-		super();
-		this.state = {
-			userProfile: []
-		};
+  constructor() {
+    super();
+    this.state = {
+      userProfile: {
+        name: '',
+        surname: '',
+        email: '',
+        phone: '',
+        username: ''
+      }
+    };
 
-		this.handleChange = this.handleChange.bind(this);
-		this.handleOnSubmit = this.handleOnSubmit.bind(this);
-	}
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
+  componentDidMount() {
+    const { userId } = this.props;
+    axios
+      .get(`http://127.0.0.1:8000/api/user_ids/${userId}`, { headers: { Accept: 'application/json' } })
+      .then((res) => this.setState({ userProfile: res.data }));
+  }
 
+  // componentDidUpdate() {
+  //   const { userId } = this.props;
+  //   axios
+  //     .get(`http://127.0.0.1:8000/api/user_ids/${userId}`, { headers: { Accept: 'application/json' } })
+  //     .then((res) => this.setState({ userProfile: res.data }));
+  // }
 
-	//TO DO - CHANGE ID WITH TOKEN OF SESSION
-	componentDidMount() {
-		/* const { id } = this.props; */
-		axios
-			//.get(`https://cnc-api.herokuapp.com/user_ids/${id}`)
-			.get(`http://127.0.0.1:8000/api/user_ids/1`, { headers: { Accept: 'application/json' } })
-			.then((res) => this.setState({ userProfile: res.data }));
-	}
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   return this.props.userId !== nextProps.userId;
+  // }
 
-	handleChange(event) {
-		this.setState({ userProfile: this.props.data });
-	}
+  handleSubmit(e) {
+    e.preventDefault();
+    const data = getValuesFrom(...e.target.elements);
+    data.phone = parseInt(data.phone);
+    const { userId } = this.props;
+    axios
+      .put(`http://127.0.0.1:8000/api/user_ids/${userId}`, data, {
+        headers: {
+          Accept: 'application/json',
+          Authorization: 'Bearer ' + AuthApi.getToken()
+        }
+      })
+      .then((res) => this.setState({ userProfile: res.data }));
+  }
 
-
-	handleOnSubmit(event) {
-		event.preventDefault();
-		axios
-			.put(`http://127.0.0.1:8001/api/directories/1`, { headers: { Accept: 'application/json' } })
-			.then(() => this.setState({ userProfile: this.props.data }));
-	}
-
-
-
-	render() {
-		return (
-			<Fragment>
-				<ResponsiveLayout />
-				<div className={styles.head}>
-					<div className="row">
-						<div className="col-lg-8 offset-lg-1">
-							<h3>Mon compte</h3>
-							<ProfileForm profileForm={this.state.userProfile} onSubmit={this.handleOnSubmit} />
-						</div>
-					</div>
-				</div>
-			</Fragment>
-		);
-	}
+  render() {
+    return (
+      <Fragment>
+        <ResponsiveLayout />
+        <div className={styles.head}>
+          <div className="row">
+            <div className="col-lg-8 offset-lg-1">
+              <h3>Mon compte</h3>
+              <ProfileForm initialValues={this.state.userProfile} onSubmit={this.handleSubmit} />
+            </div>
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
 }
 
 export default ProfileClient;
