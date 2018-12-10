4bf465c78727a7ca56fa80e10b3c5220287c3569
Karnak19 <basile64.v@gmail.com>
2018-12-10 10:28:51 +0100

    commit

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
-import Home from './pages/Home.page.jsx';
-import Error from './pages/Error.page.jsx';
-import ProfileClient from './pages/ProfileClients/ProfileClient.page.jsx';
-import SalonView from './pages/ProfileSalons/SalonView.jsx';
+import Home from "./pages/Home.page.jsx";
+import Error from "./pages/Error.page.jsx";
+import ProfileClient from "./pages/ProfileClients/ProfileClient.page.jsx";
+import SalonView from "./pages/ProfileSalons/SalonView.jsx";
 import SearchList from "./pages/SearchList/SearchList.page.jsx";
 
-export default function ({ login }) {
-    return (
-        <Router>
-            <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/login" exact component={Home} />
-                <Route path="/logout" exact component={Home} />
-                <Route path="/profile/" component={ProfileClient} />
-                <Route path="/salons/search" component={SearchList} />
-                <Route path="/salons/:id_:name/view" component={({ match }) => (
-                    <SalonView id={match.params.id_} />
-                )} />
-                {/* <Route
+export default function({ login }) {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" exact component={Home} />
+        <Route path="/logout" exact component={Home} />
+        <Route path="/profile/" component={ProfileClient} />
+        <Route path="/salons/search" component={SearchList} />
+        <Route
+          path="/salons/:id_:name/view"
+          component={({ match }) => <SalonView id={match.params.id_} />}
+        />
+        {/* <Route
                     exact path="/sprofile/:id"
                     component={({ match }) => (
                         <ProfileSalon id={match.params.id} />
@@ -27,8 +28,8 @@ export default function ({ login }) {
                 />
                 <Route path="/search/" component={SearchList} />
                 <Route path="/reactcpanel/" component={AdminPanel} /> */}
-                <Route component={Error} />
-            </Switch>
-        </Router>
-    );
+        <Route component={Error} />
+      </Switch>
+    </Router>
+  );
 }
