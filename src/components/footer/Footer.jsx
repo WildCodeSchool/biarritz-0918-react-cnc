46187b6070c3645381e5b99864f28aa679abbd2d
Basile <basile64.v@gmail.com>
2018-12-15 21:04:05 +0100

    stylized footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,37 +6,38 @@ import styles from './Footer.module.css';
 class TheFooter extends React.Component {
     render(){
         return(
-            <Fragment>
-                <footer className={styles.bgcolor}>
+            <Fragment >
+                <footer style={{backgroundColor:'#343a40', padding:'25px'}}>
                     <div className="container">
-                        <ul className="list-unstyled list-inline text-center">
-                            <li className="list-inline-item">
-                                <a className="btn-floating btn-fb mx-1">
-                                    <FontAwesomeIcon icon={['fab', 'twitter-square']} size="2x" color="" />
-                                </a>
-                            </li>
-                            <li className="list-inline-item">
-                                <a className="btn-floating btn-tw mx-1">
-                                    <FontAwesomeIcon icon={['fab', 'facebook-square']} size="2x" />
-                                </a>
-                            </li>
-                            <li className="list-inline-item">
-                                <a className="btn-floating btn-gplus mx-1">
-                                    <FontAwesomeIcon icon={['fab', 'linkedin']} size="2x" />
-                                </a>
-                            </li>
-                            <li className="list-inline-item">
-                                <a className="btn-floating btn-li mx-1">
-                                    <FontAwesomeIcon icon={['fab', 'twitter-square']} />
-                                </a>
-                            </li>
-                            <li className="list-inline-item">
-                                <a className="btn-floating btn-dribbble mx-1">
-                                    <FontAwesomeIcon icon={['fab', 'twitter-square']} />
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
+                        <div className="row">
+                            <div className="col-md-3" style={{marginTop:'15px'}}>
+                                <h3>About Us</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    Pellentesque imperdiet consectetur dolor in elementum.</p>
+                            </div>
+                            <div className="col-md" style={{marginTop:'15px'}}>
+                                <ul className="list-unstyled list-inline" style={{display:'flex', justifyContent:'space-evenly'}}>
+                                    <li className="list-inline-item">
+                                        <a href="#"><FontAwesomeIcon icon={['fab', 'twitter-square']} size="2x" /></a>
+                                    </li>
+                                    <li className="list-inline-item">
+                                        <a href="https://www.facebook.com/clicetcoupe/"><FontAwesomeIcon icon={['fab', 'facebook-square']} size="2x" /></a>
+                                    </li>
+                                    <li className="list-inline-item">
+                                        <a href="https://www.linkedin.com/company/clicetcoupe/"><FontAwesomeIcon icon={['fab', 'linkedin']} size="2x" /></a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="col-md-3" style={{marginTop:'15px'}}>
+                                <h5><strong>Contact Info</strong></h5>
+                                <p><strong>Adress:</strong> 514 S. Magnolia St.
+                                    Orlando, FL 32806
+                                </p>
+                                <p><strong>Email:</strong> contact@clicetcoupe.com</p>
+                                <p><strong>Tel.:</strong> (+33) 6 12 34 56 78</p>
+                            </div>
+                        </div>
+                    </div>               
                     <div className="footer-copyright text-center py-3">© 2018 Copyright:
                         <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
                     </div>
