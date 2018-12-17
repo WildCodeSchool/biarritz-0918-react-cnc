72ccb95fcce7e3657750ae4f32d51e9a92e887be
vianney <vianneysonneville4@gmail.com>
2018-12-17 11:57:05 +0100

    add agenda

--- src/pages/ProfileClients/ProfileClient.page.jsx
+++ src/pages/ProfileClients/ProfileClient.page.jsx
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React, { Component } from 'react';
+import axios from 'axios';
 
-import ProfileForm from "./ProfileForm.jsx";
-import styles from "./ProfileClient.module.css";
-import ResponsiveLayout from "../../layouts/Responsive.layout.jsx";
-import * as AuthApi from "../../Auth.api.js";
+import ProfileForm from './ProfileForm.jsx';
+import styles from './ProfileClient.module.css';
+import ResponsiveLayout from '../../layouts/Responsive.layout.jsx';
+import * as AuthApi from '../../Auth.api.js';
 
 function getValuesFrom(...inputs) {
    let res = {};
-   inputs.forEach(input => input.name && (res[input.name] = input.value));
+   inputs.forEach((input) => input.name && (res[input.name] = input.value));
    return res;
 }
 
@@ -16,30 +16,30 @@ class ProfileClient extends Component {
    constructor(props) {
       super(props);
       this.state = {
-         id: "",
-         name: "",
-         surname: "",
-         email: "",
-         phone: "",
-         username: "",
-         password: "",
-         sexe: "",
-         city: ""
+         id: '',
+         name: '',
+         surname: '',
+         email: '',
+         phone: '',
+         username: '',
+         password: '',
+         sexe: '',
+         city: ''
       };
 
       this.handleSubmit = this.handleSubmit.bind(this);
    }
    componentDidMount() {
       AuthApi.getUserId()
-         .then(response =>
+         .then((response) =>
             axios.get(AuthApi.SERVER + `/api/user_ids/${response.id}`, {
-               headers: { Accept: "application/json" }
+               headers: { Accept: 'application/json' }
             })
          )
-         .then(res => {
+         .then((res) => {
             this.setState({ ...res.data });
             return axios.get(AuthApi.SERVER + `${res.data.city}`, {
-               headers: { Accept: "application/json" }
+               headers: { Accept: 'application/json' }
             });
             //.then((ville) => console.log(ville));
          })
@@ -51,15 +51,15 @@ class ProfileClient extends Component {
       const data = getValuesFrom(...e.target.elements);
       data.phone = parseInt(data.phone);
       AuthApi.getUserId()
-         .then(response =>
-            axios.put(AuthApi.SERVER + `/api/user_ids/${response}`, data, {
+         .then((response) =>
+            axios.put(AuthApi.SERVER + `/api/user_ids/${response.id}`, data, {
                headers: {
-                  Accept: "application/json",
-                  Authorization: "Bearer " + AuthApi.getToken()
+                  Accept: 'application/json',
+                  Authorization: 'Bearer ' + AuthApi.getToken()
                }
             })
          )
-         .then(res => this.setState({ ...res.data }));
+         .then((res) => this.setState({ ...res.data }));
    }
 
    render() {
@@ -68,10 +68,7 @@ class ProfileClient extends Component {
             <div className="row">
                <div className="col-lg-6 offset-lg-3 col-sm-8 offset-sm-2 col-xs-8 offset-xs-2">
                   <h3>Mon compte</h3>
-                  <ProfileForm
-                     initialValues={this.state}
-                     onSubmit={this.handleSubmit}
-                  />
+                  <ProfileForm initialValues={this.state} onSubmit={this.handleSubmit} />
                </div>
             </div>
          </ResponsiveLayout>
